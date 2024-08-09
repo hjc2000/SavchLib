2ec43216fe708e2f584cf46756579fd7c0698e44
hjc2000 <hjc.666@foxmail.com>
2024-08-09 09:32:22 +0800

    Sdv3Address

--- SavchLib/Address/Sdv3Address.cs
+++ SavchLib/Address/Sdv3Address.cs
@@ -1,22 +1,38 @@
 ﻿namespace SavchLib.Address;
-public class Sdv3Address
+public static class Sdv3Address
 {
 	/// <summary>
 	///		获取 EI 的地址
 	/// </summary>
-	/// <param name="ei_index"></param>
+	/// <param name="index"></param>
 	/// <returns></returns>
-	public uint EI(uint ei_index)
+	public static uint EI(uint index)
 	{
-		if (ei_index >= 1 && ei_index <= 5)
+		if (index >= 1 && index <= 5)
 		{
-			return 0x400 + ei_index - 1;
+			return 0x400 + index - 1;
 		}
-		else if (ei_index >= 9 && ei_index <= 24)
+		else if (index >= 9 && index <= 24)
 		{
-			return 0x208 + ei_index - 9;
+			return 0x208 + index - 9;
 		}
 
-		throw new ArgumentOutOfRangeException($"{nameof(ei_index)} 超出范围。");
+		throw new ArgumentOutOfRangeException($"{nameof(index)} 超出范围。");
+	}
+
+	/// <summary>
+	///		lua 脚本 D 变量的地址。
+	/// </summary>
+	/// <param name="index"></param>
+	/// <returns></returns>
+	/// <exception cref="ArgumentOutOfRangeException"></exception>
+	public static uint LuaD(uint index)
+	{
+		if (index > 127)
+		{
+			throw new ArgumentOutOfRangeException($"{nameof(index)} 超出范围。");
+		}
+
+		return 0xc100 + (index * 2);
 	}
 }
